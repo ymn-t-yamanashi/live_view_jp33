b783bcd96a889aa3dba4c164e6e65ef78014898f
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2025-05-25 08:31:16 +0900

    Dify作成

---
+++ lib/dify.ex
@@ -0,0 +1,41 @@
+defmodule Dify do
+  @moduledoc """
+  Documentation for `Dify`.
+  """
+
+  @api "app-NlGpxKmJ8NFhWRWSDbWk8IRW"
+  @host "10.1.1.3:8001"
+
+  # Dify.llm("車は速い")
+  # Dify.llm("車は速い", self())
+  def llm(str, pid) do
+    headers = [
+      "Content-Type": "application/json",
+      Authorization: "Bearer #{@api}"
+    ]
+
+    json = """
+    {
+      "inputs": {"in": "#{str}"},
+      "response_mode": "streaming",
+      "user": "abc-123"
+    }
+    """
+
+    "http://#{@host}/v1/workflows/run"
+    |> Req.post!(headers: headers, body: json, connect_options: [timeout: 1_000_000], into: :self)
+    |> then(fn x -> x.body end)
+    |> Stream.map(fn x -> text(x, pid) end)
+    |> Stream.run()
+  end
+
+  defp text(x, pid) do
+    text =
+      Regex.replace(~r/^data: /, x, "")
+      |> Jason.decode!()
+      |> Map.get("data")
+      |> Map.get("text")
+
+    Process.send(pid, {:message, text}, [:noconnect])
+  end
+end
--- mix.exs
+++ mix.exs
@@ -54,7 +54,8 @@ defmodule LiveViewJp33.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:req, "~> 0.5.8"}
     ]
   end
 
