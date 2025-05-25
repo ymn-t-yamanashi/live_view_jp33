1e89493c34c1adbb601652a30c288553036bd5ab
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2025-05-25 09:16:56 +0900

    画面作成

--- lib/live_view_jp33_web/live/llm/index.ex
+++ lib/live_view_jp33_web/live/llm/index.ex
@@ -3,6 +3,16 @@ defmodule LiveViewJp33Web.Llm.Index do
 
   @impl true
   def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(text: "Elixirについて教えて")
+      |> assign(llm_text: "😺")
     {:ok, socket}
+  end
+
+  @impl true
+  def handle_event("llm", _value, socket) do
+
+    {:noreply, socket}
   end
 end
